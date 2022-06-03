4039f78d64d73b48c8a22431ea5205ec451a493d
OpenSauce04 <opensauce04@gmail.com>
2022-06-03 23:32:29 +0100

    Added selection

--- Program.cs
+++ Program.cs
@@ -4,9 +4,37 @@ namespace IsaacsSuitcase
 {
     internal class Program
     {
+        private static String input;
         static void Main(string[] args)
         {
+            selectionLoop:
+            Console.Clear();
             Console.WriteLine(FiggleFonts.Ivrit.Render("Isaac's Suitcase"));
+            Console.WriteLine("1: Extract save");
+            Console.WriteLine("2: Inject save");
+            Console.Write(">");
+            input = Console.ReadLine();
+            switch(input)
+            {
+                case "1":
+                    Extract();
+                    break;
+                case "2":
+                    Inject();
+                    break;
+                default:
+                    goto selectionLoop;
+            }
+        }
+
+        static void Extract()
+        {
+
+        }
+
+        static void Inject()
+        {
+
         }
     }
 }
